5bd3695720d358323346b36f96f7c292cfbf5aad
CHILDFUN <xnxx1jhonypornhub@gmail.com>
2026-07-18 13:43:04 +0100

    Create js/app.js

---
+++ js/app.js
@@ -0,0 +1,19 @@
+console.log("SOL HUB Loaded");
+
+const cards=document.querySelectorAll(".card");
+
+cards.forEach(card=>{
+
+card.addEventListener("mouseenter",()=>{
+
+card.style.boxShadow="0 0 20px #3b82f6";
+
+});
+
+card.addEventListener("mouseleave",()=>{
+
+card.style.boxShadow="none";
+
+});
+
+});
